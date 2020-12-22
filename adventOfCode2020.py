69f0bd6f234cb08e046e54adcd39edfad37143a1
cybertengu <davidhampton@live.com>
2020-12-22 14:04:43 -0700

    Added the rest of the code I did so far. I wasn't updating because I didn't want to provide answer for the days that I was working on. Now that I am behind, it should be safe to upload my solutions now.

--- adventOfCode2020.py
+++ adventOfCode2020.py
@@ -5,15 +5,15 @@ from day4 import SolveDay4
 from day5 import SolveDay5
 from day6 import SolveDay6
 from day7 import SolveDay7
-#from day8 import SolveDay8
-#from day9 import SolveDay9
-#from day10 import SolveDay10
-#from day11 import SolveDay11
-#from day12 import SolveDay12
-#from day13 import SolveDay13
-#from day14 import SolveDay14
-#from day15 import SolveDay15
-#from day16 import SolveDay16
+from day8 import SolveDay8
+from day9 import SolveDay9
+from day10 import SolveDay10
+from day11 import SolveDay11
+from day12 import SolveDay12
+from day13 import SolveDay13
+from day14 import SolveDay14
+from day15 import SolveDay15
+from day16 import SolveDay16
 #from day17 import SolveDay17
 #from day18 import SolveDay18
 #from day19 import SolveDay19
@@ -34,6 +34,37 @@ from day7 import SolveDay7
 #SolveDay6("day6.txt")
 #SolveDay7("day7test.txt")
 #SolveDay7("day7test2.txt")
-SolveDay7("day7.txt")
+#SolveDay7("day7.txt")
+#SolveDay8("day8test.txt")
+#SolveDay8("day8.txt")
+#SolveDay9("day9test.txt", 5)
+#SolveDay9("day9.txt", 25)
+#SolveDay10("day10test.txt")
+#SolveDay10("day10test2.txt")
+#SolveDay10("day10.txt")
+#SolveDay11("day11test.txt")
+#SolveDay11("day11.txt")
+#SolveDay12("day12test.txt")
+#SolveDay12("day12.txt")
+#SolveDay13('day13test.txt')
+#SolveDay13('day13test2.txt')
+#SolveDay13('day13test3.txt')
+#SolveDay13('day13test4.txt')
+#SolveDay13('day13test5.txt')
+#SolveDay13('day13test6.txt')
+#SolveDay13('day13.txt')
+#SolveDay14('day14.txt')
+#SolveDay14('day14test.txt')
+#SolveDay14('day14test2.txt')
+#SolveDay15('day15test.txt')
+#SolveDay15('day15test2.txt')
+#SolveDay15('day15test3.txt')
+#SolveDay15('day15test4.txt')
+#SolveDay15('day15test5.txt')
+#SolveDay15('day15test6.txt')
+#SolveDay15('day15test7.txt')
+#SolveDay15('day15test8.txt')
+#SolveDay15('day15.txt')
+SolveDay16('day16.txt')
 
 print("Finished running.")
